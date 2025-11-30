1389bd0c39b0ac6e47155f3651ea369e9da6464e
Nazar Kolesnikov <nazarkolesnikov148@ukr.net>
2025-11-30 05:58:40 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Main from "./pages/Main/Main.jsx";
 import AccountControl from "./pages/AccountControl/AccountControl.jsx";
